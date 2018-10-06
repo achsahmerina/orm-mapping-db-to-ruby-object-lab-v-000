0147f29da78873789aa37c9aa17954d393bb18e6
Achsah Abraham <achsahmp@gmail.com>
2018-10-06 23:15:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,7 +92,7 @@ class Student
 
   def self.all_students_in_grade_X(num)
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade = ?;
+    SELECT * FROM students WHERE students.grade = ?
     SQL
     DB[:conn].execute(sql, num)
   end
