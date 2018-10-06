e98de03d84c402ac22e2bd95067f7366ab097f0b
Achsah Abraham <achsahmp@gmail.com>
2018-10-06 23:14:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,13 +74,12 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade = ? LIMIT 1
+    SELECT * FROM students WHERE grade=10 ORDER BY students.id LIMIT ?;
     SQL
-    DB[:conn].execute(sql).collect do |row|
-      self.new_from_db(row)
-    end.first
+    DB[:conn].execute(sql, num)
+    end
   end
 
   def self.first_student_in_grade_10
