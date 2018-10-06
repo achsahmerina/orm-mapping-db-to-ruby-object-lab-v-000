28f1519faa7609e72e20eb302a9a3946117bfc11
Achsah Abraham <achsahmp@gmail.com>
2018-10-06 22:57:14 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,7 @@ class Student
 
   def self.all_students_in_grade_X
     sql = <<-SQL
-    SELECT * FROM students WHERE students.grade X
+    SELECT * FROM students WHERE students.grade 
     SQL
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
